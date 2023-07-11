984f8496465308474729b9b17692608222fc9740
Jorge André <jorge.matosandre@gmail.com>
2023-07-11 17:01:01 +0200

    Move rejseplanen api call to background service

---
+++ B-Q01/BackgroundServices/NextDeparturesService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace B_Q01.BackgroundServices
+{
+    public class NextDeparturesService : BackgroundService
+    {
+        private readonly ILogger<NextDeparturesService> logger;
+
+        public NextDeparturesService(ILogger<NextDeparturesService> logger)
+        {
+            this.logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            await DoWork();
+
+            using PeriodicTimer timer = new(TimeSpan.FromMinutes(1));
+            try
+            {
+                while (await timer.WaitForNextTickAsync(cancellationToken))
+                {
+                    await DoWork();
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                logger.LogInformation("NextDepartures hosted service is stopping.");
+            }
+        }
+
+        private async Task DoWork()
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var currentDate = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, 
+                TimeZoneInfo.FindSystemTimeZoneById("Romance Standard Time"));
+            var dateString = string.Join(".", currentDate.ToString("dd"), currentDate.ToString("MM"), currentDate.ToString("yy"));
+
+            var res = await client.GetStringAsync("http://xmlopen.rejseplanen.dk/bin/rest.exe/departureBoard?id=2753&date="+dateString+"&offsetTime=0&format=json");
+            var board = JsonNode.Parse(res)?["DepartureBoard"]?["Departure"]?.ToString();
+
+            IList<Departure> departures = new List<Departure>();
+            if (board != null)
+                departures = JsonSerializer.Deserialize<List<Departure>>(board)!;
+
+            Console.WriteLine("Next bus: " + departures.First().ToString());
+        }
+    }
+}
--- B-Q01/Program.cs
+++ B-Q01/Program.cs
@@ -1,32 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using B_Q01;
+using B_Q01.BackgroundServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Collections.ObjectModel;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
-using HttpClient client = new();
-client.DefaultRequestHeaders.Accept.Clear();
-client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+var builder = new HostBuilder();
 
-var res = await client.GetStringAsync("http://xmlopen.rejseplanen.dk/bin/rest.exe/departureBoard?id=2753&date=10.07.23&offsetTime=1&format=json");
-var board = JsonNode.Parse(res)?["DepartureBoard"]?["Departure"]?.ToString();
-
-IList<Departure> departures = new List<Departure>();
-if (board != null)
-    departures = JsonSerializer.Deserialize<List<Departure>>(board)!;
-
-List<Departure> tracking = new List<Departure>();
-
-foreach (var departure in departures)
-{
-    if (!tracking.Any(x => x.TimeUtc.Equals(departure.TimeUtc)))
+builder.ConfigureServices(services =>
     {
-        tracking.Add(departure);
-    }
-    var trkDeparture = tracking.Find(x => x.TimeUtc.Equals(departure.TimeUtc))!;
-    if (!departure.Equals(trkDeparture))
-    {
-        tracking[tracking.FindIndex(d => d.Equals(trkDeparture))] = departure;
-    }
-}
+        services.AddHostedService<NextDeparturesService>();
+        //services.AddHostedService<DepartureAlertService>();
 
+        services.AddLogging();
+    });
+builder.ConfigureLogging(configLogging =>
+    {
+        configLogging.AddConsole();
+        configLogging.AddDebug();
+    });
+
+await builder.RunConsoleAsync();
+
+//List<Departure> tracking = new();
+
+//foreach (var departure in departures)
+//{
+//    if (!tracking.Any(x => x.TimeUtc.Equals(departure.TimeUtc)))
+//    {
+//        tracking.Add(departure);
+//    }
+//    var trkDeparture = tracking.Find(x => x.TimeUtc.Equals(departure.TimeUtc))!;
+//    if (!departure.Equals(trkDeparture))
+//    {
+//        tracking[tracking.FindIndex(d => d.Equals(trkDeparture))] = departure;
+//    }
+//}
+
+//var nextArrival = tracking.First();
+//if ((nextArrival.RealTimeUtc - DateTimeOffset.UtcNow).Minutes < 10)
+//{
+//    Console.WriteLine("Bus arriving soon!");
+//    Console.WriteLine(nextArrival);
+//}
+//else
+//{
+//    Console.WriteLine("Ya got time");
+//}
