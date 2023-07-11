e332a6ed3718586178b608f93869cda5a269334a
Jorge André <jorge.matosandre@gmail.com>
2023-07-11 12:32:52 +0200

    Add Rejseplanen API call and ArrivalBoard object processing

---
+++ B-Q01/DepartureBoard.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace B_Q01
+{
+    public record class DepartureBoard(
+        [property: JsonPropertyName("Departure")] List<Departure> Departures);
+
+    public record class Departure(
+        [property: JsonPropertyName("type")] string Type,
+        [property: JsonPropertyName("line")] string Line,
+        [property: JsonPropertyName("stop")] string Stop,
+        [property: JsonPropertyName("direction")] string Direction,
+        [property: JsonPropertyName("time")] string Time,
+        [property: JsonPropertyName("rtTime")] string RealTime,
+        [property: JsonPropertyName("date")] string Date,
+        [property: JsonPropertyName("ref")] Uri DetailRef)
+    {
+        public DateTimeOffset TimeUtc
+        {
+            get
+            {
+                var parsedDate = DateTime.ParseExact(String.Join(" ", Date, Time), "dd.MM.yy HH:mm", new CultureInfo("en-DK"));
+                return new DateTimeOffset(parsedDate);
+            }
+        }
+
+        public DateTimeOffset RealTimeUtc
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(RealTime))
+                    return TimeUtc;
+                var parsedDate = DateTime.ParseExact(String.Join(" ", Date, RealTime), "dd.MM.yy HH:mm", new CultureInfo("en-DK"));
+                return new DateTimeOffset(parsedDate);
+            }
+            set
+            {
+                RealTimeUtc = value;
+            }
+        }
+
+        public TimeSpan ScheduleOffset
+        {
+            get
+            {
+                return RealTimeUtc.Subtract(TimeUtc);
+            }
+        }
+
+    }
+}
---
+++ B-Q01/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using B_Q01;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+var res = await client.GetStringAsync("http://xmlopen.rejseplanen.dk/bin/rest.exe/departureBoard?id=2753&date=10.07.23&offsetTime=1&format=json");
+var board = JsonNode.Parse(res)?["DepartureBoard"]?["Departure"]?.ToString();
+
+IList<Departure> departures = new List<Departure>();
+if (board != null)
+    departures = JsonSerializer.Deserialize<List<Departure>>(board)!;
+
+List<Departure> tracking = new List<Departure>();
+
+foreach (var departure in departures)
+{
+    if (!tracking.Any(x => x.TimeUtc.Equals(departure.TimeUtc)))
+    {
+        tracking.Add(departure);
+    }
+    var trkDeparture = tracking.Find(x => x.TimeUtc.Equals(departure.TimeUtc))!;
+    if (!departure.Equals(trkDeparture))
+    {
+        tracking[tracking.FindIndex(d => d.Equals(trkDeparture))] = departure;
+    }
+}
+
