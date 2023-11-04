f6a8b63e0c5197a1d3f69cc1e34e648562b97b40
Jorge André <jorge.matosandre@gmail.com>
2023-11-04 13:31:18 +0100

    Add simple kafka consumer

---
+++ Client.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "foo",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("departures");
+    try
+    {
+        while (true)
+        {
+            var result = consumer.Consume();
+            Console.WriteLine(result.Message.Value);
+        }
+    }
+    finally
+    {
+        consumer.Close();
+    }
+}
